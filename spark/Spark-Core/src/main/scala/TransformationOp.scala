86e6450ff4f0be1a75fe57050a9c99d4574ba394
wudingchao <everett@163.com>
2020-12-07 15:10:32 +0800

    TransformationOp

---
+++ spark/Spark-Core/src/main/scala/TransformationOp.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object TransformationOp {
+  val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("transformation_op");
+  val sc = new SparkContext(conf)
+
+  def main(args: Array[String]): Unit = {
+    sc.parallelize(Seq(1, 2, 3, 4, 5, 6), 2)
+      .mapPartitions(iter => {
+        iter.map(item => item * 10)
+        iter
+      }).collect()
+      .foreach(item => println(item))
+  }
+
+  def mapPartitionsWithIndex(): Unit = {
+    sc.parallelize(Seq(1, 2, 3, 4, 5, 6), 2)
+      .mapPartitionsWithIndex((index, iter)=>{
+      println("index" + index)
+        iter.foreach(item=>println(item))
+        iter
+        })
+      .collect()
+  }
+
+}
